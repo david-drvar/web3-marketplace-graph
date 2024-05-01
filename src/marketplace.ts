fa158d087da941ac225407d1c6359710e757c4d4
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-05-01 18:43:08 +0200

    handle item update event

--- src/marketplace.ts
+++ src/marketplace.ts
@@ -1,6 +1,7 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
-import { ItemBought as ItemBoughtEvent, ItemDeleted as ItemDeletedEvent, ItemListed as ItemListedEvent } from "../generated/Marketplace/Marketplace";
+import { ItemBought as ItemBoughtEvent, ItemDeleted as ItemDeletedEvent, ItemListed as ItemListedEvent, ItemUpdated, ItemUpdated as ItemUpdatedEvent } from "../generated/Marketplace/Marketplace";
 import { Item } from "../generated/schema";
+import { log } from "@graphprotocol/graph-ts";
 
 export function handleItemBought(event: ItemBoughtEvent): void {
   let entity = Item.load(getIdFromEventParams(event.params.id, event.params.seller));
@@ -12,6 +13,8 @@ export function handleItemBought(event: ItemBoughtEvent): void {
   entity.itemStatus = "Bought";
 
   entity.save();
+
+  log.info("Item with id {} bought", [entity.id]);
 }
 
 export function handleItemDeleted(event: ItemDeletedEvent): void {
@@ -23,6 +26,8 @@ export function handleItemDeleted(event: ItemDeletedEvent): void {
   entity.itemStatus = "Deleted";
 
   entity.save();
+
+  log.info("Item with id {} deleted", [entity.id]);
 }
 
 export function handleItemListed(event: ItemListedEvent): void {
@@ -31,8 +36,11 @@ export function handleItemListed(event: ItemListedEvent): void {
 
   let entity = Item.load(getIdFromEventParams(event.params.id, event.params.seller));
   if (!entity) {
-    entity = new Item(getIdFromEventParams(event.params.id, event.params.seller));
+    log.error("Item with id {} already exists.", [getIdFromEventParams(event.params.id, event.params.seller)]);
+    return;
   }
+
+  entity = new Item(getIdFromEventParams(event.params.id, event.params.seller));
 
   entity.buyer = Address.fromString("0x000000000000000000000000000000000000dEaD"); // dead address, nobody bought it yet
   entity.seller = entity.seller = event.params.seller;
@@ -47,8 +55,31 @@ export function handleItemListed(event: ItemListedEvent): void {
   entity.transactionHash = event.transaction.hash;
 
   entity.save();
+
+  log.info("Item with id {} listed", [entity.id]);
 }
 
 function getIdFromEventParams(id: BigInt, sellerAddress: Address): string {
   return id.toString(); //+ sellerAddress.toHexString();
+}
+
+export function handleItemUpdated(event: ItemUpdatedEvent): void {
+  let entity = Item.load(getIdFromEventParams(event.params.id, event.params.seller));
+  if (!entity) {
+    log.error("Item with id {} doesn't exist. Error during handleItemUpdated.", [getIdFromEventParams(event.params.id, event.params.seller)]);
+    return;
+  }
+
+  entity.title = event.params.title;
+  entity.description = event.params.description;
+  entity.price = event.params.price;
+  entity.photosIPFSHashes = event.params.photosIPFSHashes;
+
+  // entity.blockNumber = event.block.number;
+  // entity.blockTimestamp = event.block.timestamp;
+  // entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+
+  log.info("Item with id {} updated", [entity.id]);
 }
