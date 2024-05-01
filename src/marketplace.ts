096276673cff3ea7eedfbca58b4d0378d3320316
david-drvar <david.drvar.bogdanovic@gmail.com>
2024-05-01 18:03:05 +0200

    graph schema item v0.0.4

--- src/marketplace.ts
+++ src/marketplace.ts
@@ -1,52 +1,54 @@
-import {
-  ItemBought as ItemBoughtEvent,
-  ItemDeleted as ItemDeletedEvent,
-  ItemListed as ItemListedEvent
-} from "../generated/Marketplace/Marketplace"
-import { ItemBought, ItemDeleted, ItemListed } from "../generated/schema"
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { ItemBought as ItemBoughtEvent, ItemDeleted as ItemDeletedEvent, ItemListed as ItemListedEvent } from "../generated/Marketplace/Marketplace";
+import { Item } from "../generated/schema";
 
 export function handleItemBought(event: ItemBoughtEvent): void {
-  let entity = new ItemBought(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.Marketplace_id = event.params.id
-  entity.seller = event.params.seller
-  entity.buyer = event.params.buyer
+  let entity = Item.load(getIdFromEventParams(event.params.id, event.params.seller));
+  if (!entity) {
+    return;
+  }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.buyer = event.params.buyer;
+  entity.itemStatus = "Bought";
 
-  entity.save()
+  entity.save();
 }
 
 export function handleItemDeleted(event: ItemDeletedEvent): void {
-  let entity = new ItemDeleted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.Marketplace_id = event.params.id
-  entity.seller = event.params.seller
+  let entity = Item.load(getIdFromEventParams(event.params.id, event.params.seller));
+  if (!entity) {
+    return;
+  }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.itemStatus = "Deleted";
 
-  entity.save()
+  entity.save();
 }
 
 export function handleItemListed(event: ItemListedEvent): void {
-  let entity = new ItemListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.Marketplace_id = event.params.id
-  entity.seller = event.params.seller
-  entity.title = event.params.title
-  entity.description = event.params.description
-  entity.price = event.params.price
+  // let entity = new Item(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  // entity.id = event.params.id;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let entity = Item.load(getIdFromEventParams(event.params.id, event.params.seller));
+  if (!entity) {
+    entity = new Item(getIdFromEventParams(event.params.id, event.params.seller));
+  }
 
-  entity.save()
+  entity.buyer = Address.fromString("0x000000000000000000000000000000000000dEaD"); // dead address, nobody bought it yet
+  entity.seller = entity.seller = event.params.seller;
+  entity.title = event.params.title;
+  entity.description = event.params.description;
+  entity.price = event.params.price;
+  entity.photosIPFSHashes = event.params.photosIPFSHashes;
+  entity.itemStatus = "Listed";
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+function getIdFromEventParams(id: BigInt, sellerAddress: Address): string {
+  return id.toString(); //+ sellerAddress.toHexString();
 }
